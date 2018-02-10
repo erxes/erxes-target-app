55af69e8f4f37f59defd398f6c2b9ca3ed6fbbfc
munkhdul <munkhdul.e@nmma.co>
2018-02-10 19:39:24 +0800

    instruction text

--- src/App.js
+++ src/App.js
@@ -11,13 +11,6 @@ function App({ user }) {
             <li>Click Erxes help button below.</li>
             <li>To create a new message click on the [+] button.</li>
             <li>Send a message.</li>
-            <li>
-              <a href="https://demo.erxes.io/" target="_blank">
-                Go to the Erxes app
-              </a>{' '}
-              and sign in with following account. (Email: demo@erxes.io,
-              Password: demo)
-            </li>
             <li>You can find your sent messages in the Inbox.</li>
           </ul>
         </div>
